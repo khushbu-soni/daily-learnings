a6c4a93cc04d2a9c23bb5f937bc3325d4b979d69
Khushbu <soni.khushbu2104@gmail.com>
2024-08-29 02:20:12 +0530

    practicing Multi threading

--- src/main/java/org/example/matrixDaigonalSum.java
+++ src/main/java/org/example/matrixDaigonalSum.java
@@ -6,15 +6,28 @@ public class matrixDaigonalSum {
 
 
     public static void main(String[] args) {
-        int [][] mat= new int[][]{{1, 2}, {4, 5}};
+        int [][] mat= new int[][]{{1, 0}, {0,1}};
             int[] originalArray={2,2,3,4};
 //        int sum = daigonalSum(mat);
+        int mul = multiplicationsOfDaigonal(mat);
         int[][] ints = construct2DArray(originalArray, 2, 2);
+        int[] ints1 = runnignSum1DArray(originalArray);
+        for (int i : ints1) {
+            System.out.println(i);
+        }
+
+//        int speciaPositionBF = findSpeciaPositionBF(mat);
+        int speciaPosition = findSpeciaPosition(mat);
+//        System.out.println("speciaPositionBF "+speciaPositionBF);
+        System.out.println("speciaPosition "+speciaPosition);
+
+
         for (int i=0;i<ints.length;i++){
             for (int j=0;j<ints.length;j++)
                 System.out.println(ints[i][j]);
         }
 //        System.out.println(sum);
+        System.out.println(mul);
     }
 
     public static  int daigonalSum(int [][] mat){
@@ -32,6 +45,20 @@ public class matrixDaigonalSum {
 
     }
 
+    public static int multiplicationsOfDaigonal(int [][] mat){
+        int leftDiagonal=1;
+        int righDiagonal=1;
+        int n =mat.length;
+        for(int i =0; i<n ;i++){
+
+            leftDiagonal*=mat[i][i];
+            righDiagonal*=mat[i][n-i-1];
+
+        }
+
+        return leftDiagonal*righDiagonal;
+    }
+
     public static int[][] construct2DArray(int[] originalArray, int row, int col){
         if(originalArray.length!=row*col)
             return new int[0][0];
@@ -42,5 +69,97 @@ public class matrixDaigonalSum {
         }
 
         return newArray;
+    }
+
+    public static int findSpeciaPositionBF(int [][] mat){
+
+        int result=0;
+
+//        find the length of row
+        int lengthOfRow=mat.length;
+        int lengthOfCol=mat[0].length;
+        boolean specialPosition = true;
+//        For Each Row and colmn check the spacial positions
+
+        for (int row =0; row<lengthOfRow; row++){
+            for (int col=0; col<lengthOfCol; col++){
+
+                if(mat[row][col]==0) continue;
+
+//                check for all row and col
+                for(int r=0;r<lengthOfRow;r++){
+                    if (r != row && mat[r][col] == 1) {
+                        specialPosition = false;
+                        break;
+                    }
+
+                }
+
+                for(int c=0; c< lengthOfCol; c++) {
+                    if (c != col && mat[c][row] == 1) {
+                        specialPosition = false;
+                        break;
+
+                    }
+                }
+                if(specialPosition)
+                    result++;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static int findSpeciaPosition(int [][] mat){
+        int result=0;
+
+        int lengthOfRow=mat.length;
+        int lengthOfColumn=mat[0].length;
+
+        int[] rowCount=new int[lengthOfRow];
+        int[] colCount=new int[lengthOfColumn];
+
+        for(int row=0;row<lengthOfRow;row++){
+            for (int col=0;col<lengthOfColumn;col++){
+
+                if(mat[row][col]==1){
+
+                    rowCount[row]++;
+                    colCount[col]++;
+
+                }
+
+
+
+            }
+        }
+
+        for (int row =0;row<lengthOfRow;row++) {
+            for (int col = 0; col < lengthOfColumn; col++) {
+
+                if (mat[row][col] == 0)
+                    continue;
+
+                if (rowCount[row] == 1 && colCount[col] == 1) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+
+    public  static int[] runnignSum1DArray(int[] array){
+        int sum=0;
+        int lenghtOfArray=array.length;
+        for(int element=0; element<lenghtOfArray;element++){
+
+            sum+=array[element];
+            array[element]=sum;
+        }
+        return array;
     }
 }
